9d69b787372dc66a49eabeb2fa8456cebaa1e6d7
Gerd Wuetherich <gerd@gerd-wuetherich.de>
2017-10-21 13:33:16 +0200

    fixed repo location

--- .travis/artifact-upload.sh
+++ .travis/artifact-upload.sh
@@ -5,7 +5,7 @@ target_dir=slizaa-opencypher-xtext
 
 echo TRAVIS_BUILD_DIR: $TRAVIS_BUILD_DIR
 
-PRODUCTS_DIR=$TRAVIS_BUILD_DIR/releng/org.slizaa.core.p2/target/repository
+PRODUCTS_DIR=$TRAVIS_BUILD_DIR/releng/org.slizaa.neo4j.opencypher.p2/target/repository
 echo PRODUCTS_DIR: $PRODUCTS_DIR
 
 filesToUpload=$(find $PRODUCTS_DIR -name "*.jar" -printf '%P\n')
